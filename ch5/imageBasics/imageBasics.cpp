83e90695eb6b7a21d15d416f2c24f6f9bdbc0421
Mario Namtao Shianti Larcher <mnslarcher@gmail.com>
2022-08-28 16:15:38 +0200

    Add imageBasics

---
+++ ch5/imageBasics/imageBasics.cpp
@@ -0,0 +1,59 @@
+#include <chrono>
+#include <iostream>
+
+using namespace std;
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+int main(int argc, char** argv) {
+  cv::Mat image;
+  image = cv::imread(argv[1]);
+
+  if (image.data == nullptr) {
+    cerr << "File" << argv[1] << "don't exists" << endl;
+    return 0;
+  }
+
+  cout << "Image width is: " << image.cols << ", the height is: " << image.rows
+       << ", the number of channels is: " << image.channels() << endl;
+  cv::imshow("image", image);
+  cv::waitKey(0);
+
+  if (image.type() != CV_8UC1 && image.type() != CV_8UC3) {
+    cout << "Please enter a color or grayscale image." << endl;
+    return 0;
+  }
+
+  chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
+  for (size_t y = 0; y < image.rows; y++) {
+    unsigned char* row_ptr =
+        image.ptr<unsigned char>(y);  // pointer to y-th row
+    for (size_t x = 0; x < image.cols; x++) {
+      unsigned char* data_ptr =
+          &row_ptr[x * image.channels()];  // pointer to (x, y)
+      for (int c = 0; c != image.channels(); c++) {
+        unsigned char data = data_ptr[c];  // I(x, y) pixel, c channel
+      }
+    }
+  }
+  chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
+  chrono::duration<double> time_used =
+      chrono::duration_cast<chrono::duration<double>>(t2 - t1);
+  cout << "Time taken to traverse the image: " << time_used.count()
+       << " seconds " << endl;
+
+  cv::Mat image_another = image;
+  image_another(cv::Rect(0, 0, 100, 100)).setTo(0);
+  cv::imshow("image", image);
+  cv::waitKey(0);
+
+  cv::Mat image_clone = image.clone();
+  image_clone(cv::Rect(0, 0, 100, 100)).setTo(255);
+  cv::imshow("image", image);
+  cv::imshow("image_clone", image_clone);
+  cv::waitKey(0);
+
+  cv::destroyAllWindows();
+  return 0;
+}
